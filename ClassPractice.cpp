09fd8a460b005848f378c9bcf375825f787ed9da
Shlmp <daniel.kil@uniat.edu.mx>
2023-05-25 09:57:37 -0600

    Added (Tic Tac Toe) Beginning

--- ClassPractice.cpp
+++ ClassPractice.cpp
@@ -25,7 +25,33 @@ void display(const vector<string>& vec);
 
 int main()
 {
+    const int ROWS = 3;
+    const int COLUMNS = 3;
+    char board[ROWS][COLUMNS] = { {'O', 'X', 'O'},
+                                  {' ', 'X', 'X'},
+                                  {'X', 'O', 'O'} };
 
+    cout << "---- TIC TAC TOE ----\n";
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << endl << "---- TIC TAC TOE ----" << endl;
+    board[1][0] = 'X';
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+    cout << "X wins!!\n";
 }
 
 void MatrixBox()
